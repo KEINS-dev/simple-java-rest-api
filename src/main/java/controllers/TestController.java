5d207e7a7bc63238ad8bff7b4b1c74550257cdac
Keins <nikoloztatulashvili@ibsu.edu.ge>
2026-03-24 12:55:09 +0400

    first

---
+++ src/main/java/controllers/TestController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import ge.ibsu.demo.dto.TestPerson;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @RestController
+    @RequestMapping("/api/test")
+    public class TestController {
+        @GetMapping("/hi")
+       public String sayHi(){
+            return "hello from server";
+        }
+        @GetMapping("/call")
+        public TestPerson call(@RequestBody TestPerson p){
+            p.setFirstName(p.getFirstName()+ "from server ");
+            return p;
+        }
+        }
